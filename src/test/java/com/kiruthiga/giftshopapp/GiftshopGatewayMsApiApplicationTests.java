9c4a97924864d9a4d4e3a357ca5a06296d909cc3
kiruthiga2408 <keerthilalli24@gmail.com>
2022-05-05 21:19:46 +0530

    added

---
+++ src/test/java/com/kiruthiga/giftshopapp/GiftshopGatewayMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiruthiga.giftshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftshopGatewayMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
